dbfeca44fd88430d87214f3b810f2f65b92f9ab1
Alex Zhang <zhangchi0104@live.com>
2024-03-24 22:26:09 +1100

    initial commitment

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello() {
+    return {
+      message: 'Hello World!',
+      date: new Date().toISOString(),
+    };
+  }
+}
