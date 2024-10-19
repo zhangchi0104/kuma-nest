822c0bfb5dd6b942ef9b4197f5c2d145b7db840a
Alex Zhang <zhangchi0104@live.com>
2024-10-19 17:24:46 +1100

    chore: manage infrastructure independently

--- infrastructure/imports.tf
+++
@@ -1,9 +0,0 @@
-# import {
-#     to = aws_acm_certificate.wildcard_api_chiz_dev
-#     id = "arn:aws:acm:ap-southeast-2:211125712027:certificate/4eb6e69c-894a-4763-94dc-ac0cd8968ee6"
-# }
-
-# import {
-#     to = aws_route53_zone.api_chiz_dev
-#     id = "Z05341373E3JETY6N02KR"
-# }
