d5bcf286e93f4472f9543cda1663c84b318fbe00
Alex Zhang <zhangchi0104@live.com>
2024-11-07 14:11:28 +1100

    revert: using application service to make my

--- lib/constructs/ecs-service-stack.ts
+++ lib/constructs/ecs-service-stack.ts
@@ -2,8 +2,9 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+// import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+// import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 import { CfnOutput } from 'aws-cdk-lib';
 import { ServerProps } from 'lib/types/ServerEnvironmentVariables';
@@ -14,7 +15,9 @@ type EcsServiceStackProps = ServerProps & {
   hostedZone: route53.IHostedZone;
 };
 export class EcsServiceStack extends Construct {
-  public readonly ecsService: ecs.BaseService;
+  public readonly ecsService:
+    | ecs.BaseService
+    | ecsp.ApplicationLoadBalancedEc2Service;
   constructor(scope: Construct, id: string, props: EcsServiceStackProps) {
     const { env, vpc, hostedZone } = props;
     super(scope, id);
@@ -55,40 +58,26 @@ export class EcsServiceStack extends Construct {
       ],
     });
 
-    const service = new ecs.Ec2Service(this, 'BlogService', {
-      cluster: ecsCluster,
-      taskDefinition,
-    });
-    const loadBalancer = new elbv2.ApplicationLoadBalancer(
+    // const service = new ecs.Ec2Service(this, 'BlogService', {
+    //   cluster: ecsCluster,
+    //   taskDefinition,
+    // });
+    const service = new ecsp.ApplicationLoadBalancedEc2Service(
       this,
-      'LoadBalancer',
+      'BlogService',
       {
+        cluster: ecsCluster,
+        taskDefinition,
         vpc,
-        internetFacing: true,
+        publicLoadBalancer: true,
+        domainZone: hostedZone,
+        domainName: 'blog',
+        certificate,
       },
     );
-    const listener = loadBalancer.addListener('Listener', {
-      port: 443,
-      certificates: [certificate],
-    });
-    listener.addTargets('ECS', {
-      port: 8000,
-      targets: [
-        service.loadBalancerTarget({
-          containerName: 'BlogContainer',
-          containerPort: 8000,
-        }),
-      ],
-    });
-    new route53.ARecord(this, 'AliasRecord', {
-      zone: hostedZone,
-      target: route53.RecordTarget.fromAlias(
-        new route53Targets.LoadBalancerTarget(loadBalancer),
-      ),
-    });
     this.ecsService = service;
     new CfnOutput(this, 'LoadBalancerDNS', {
-      value: loadBalancer.loadBalancerDnsName,
+      value: service.loadBalancer.loadBalancerDnsName,
     });
   }
 }
