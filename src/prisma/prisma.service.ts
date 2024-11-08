0c1f32a40478524cf7ea38629e2952735e65f415
Alex Zhang <zhangchi0104@live.com>
2024-11-08 17:52:14 +1100

    feat (infrastructure): Updated infrastructure

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    console.log('Prisma Url: ' + process.env.DATABASE_TRANSACTION_URL);
     await this.$connect();
   }
 }
